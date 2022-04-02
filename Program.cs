a05cf2072e68322fc98c163b34c5a706272c70a9
Harman Singh <ac117072@avcol.school.nz>
2022-04-02 21:18:20 +1300

    Added questions & created "hard" method.

--- Program.cs
+++ Program.cs
@@ -18,15 +18,21 @@ namespace MaoriQuiz
         }
         static void Main(string[] args)
         {
+
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Hello and welcome to Harman Singh's multiple choice Te Reo Maori quiz! \nWhen asked a question, you must type A, B or C and then press ENTER. \n\nAre you a beginner to Maori? if so, type Y and press ENTER. If not, type N and press ENTER.");
             Console.ResetColor();
             char choice = Convert.ToChar(Console.ReadLine().ToUpper());
             Console.Clear();
 
-            if (choice == 'Y')
+            switch(choice)
             {
-                easy();
+                case 'Y': easy();
+                    break;
+                case 'N': hard();
+                    break;
+                default: easy();
+                    break;
             }
 
             static void easy()
@@ -50,9 +56,9 @@ namespace MaoriQuiz
                     Console.ResetColor();
                 }
 
-                Console.WriteLine("\nQUESTION 2: \n\nWhat is the Maori word for good morning? \n\nA) Māmāngi \nB) Mōrena \nC) Matariki\n");
+                Console.WriteLine("\nQUESTION 2: \n\nWhat is the English phrase for Morena? \n\nA) Good morning \nB) Good afternoon \nC) Good evening\n");
                 char ans2 = Convert.ToChar(Console.ReadLine().ToUpper());
-                if (ans2 == 'B')
+                if (ans2 == 'A')
                 {
                     correctanseasy();
                     score = score + 1;
@@ -67,6 +73,137 @@ namespace MaoriQuiz
                     Console.WriteLine("\nYou have a total of " + score + " points.");
                     Console.ResetColor();
                 }
+
+                Console.WriteLine("\nQUESTION 3: \n\nWhat is the Maori term for performing arts? \n\nA) Kapa haka \nB) Korare \nC) Kaumatua");
+                char ans3 = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (ans3 == 'A')
+                {
+                    correctanseasy();
+                    score = score + 2;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    wronganseasy();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("\nQUESTION 4: \n\nMatariki can be best described as... \n\nA) The Maori term for the Pleiades star cluster \nB) The beginning of the Maori New Year \nC) A celebration akin to Christmas");
+                char ans4 = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (ans4 == 'C')
+                {
+                    wronganseasy();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    correctanseasy();
+                    score = score + 2;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("\nQUESTION 5: \n\nWhat is the Maori term for kanoe? \n\nA) Whaka \nB) Raka \nC) Waka");
+                char ans5 = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (ans5 == 'C')
+                {
+                    correctanseasy();
+                    score = score + 3;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                } else
+                {
+                    wronganseasy();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("\nQUESTION 6: \n\nWhat is the English equivalent of Aoteaora? \n\nA) The Pacific Ocean \nB) New Zealand \nC) Atlantis");
+                char ans6 = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (ans6 == 'B')
+                {
+                    correctanseasy();
+                    score = score + 3;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                } else
+                {
+                    wronganseasy();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("\nQUESTION 7: \n\nGood job is to ka pai as good work is to... \n\nA) Mahi pahi \nB) Tino pai \nC) Ka rawe");
+                char ans7 = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (ans7 == 'A')
+                {
+                    correctanseasy();
+                    score = score + 4;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                } else
+                {
+                    wronganseasy();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("\nFINAL QUESTION: \n\n.. \n\nA) ... \nB) ... \nC)");
+                char ans8 = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (ans8 == 'B')
+                {
+                    correctanseasy();
+                    score = score + 4;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                } else
+                {
+                    wronganseasy();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nYou have a total of " + score + " points.");
+                    Console.ResetColor();
+                }
+                if (score <= 11)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nSorry, your score of " + score + " does not qualify as a passing score. Do you wish to have another go at this quiz? \nIf so, please type Y and press ENTER. If not, please type N and press ENTER to exit.");
+                    Console.ResetColor();
+                    char choice2 = Convert.ToChar(Console.ReadLine().ToUpper());
+                    Console.Clear();
+                    while (choice2 == 'Y')
+                    {
+                        easy();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nCongratulations! You have passed Harman Singh's multiple choice Te Reo Maori quiz with a score of " + score + "!\nDo you wish to try more advanced questions? \nIf so, please type Y and press ENTER. If not, please type N and press ENTER to leave.");
+                    Console.ResetColor();
+                    char choice3 = Convert.ToChar(Console.ReadLine().ToUpper());
+                    Console.Clear();
+                    while (choice3 == 'Y')
+                    {
+                        hard();
+                    }
+                }
+                {
+                }
+                {
+                }
+            }
+            static void hard()
+            {
+
             }
         }
     }
