3de9bd81309716e22c72e67cb4143429a2df01be
Harman Singh <ac117072@avcol.school.nz>
2022-06-04 15:30:09 +1200

    Updated pseudocode and comments.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
-﻿/* This program is a Te Reo Maori quiz written by Harman Singh.
+﻿/* This program is a Te Reo Maori quiz written by Harman Singh
 
-Start
-Ask user if they are a beginner
-If they enter Y, go to the easy method
-If they enter N, go to the hard method
-After finishing the easy set of questions, check whether the user's score is less than or equal to 3
-IF so, ask user if they want to try the easy questions again or end the program
-WHILE they enter Y, go back to the start of the easy method
-ELSE, if the user's score is equal to or more than 4, ask user if they want to try hard questions or end the program
-WHILE they enter Y, go to the hard method
-After finishing the hard set of questions, check whether the user's score is less than or equal to 9
-IF so, ask user if they want to try the easy questions of end the program
-WHILE they enter Y, go back to the easy method
-ELSE, if the user's score is equal to or more than 10, congratulate the user and end the program
-End
+START
+Ask user if they are a beginner to Te Reo Maori
+If the user enters Y, go to the "easy" method
+If the user enters N, go to the "hard" method
+If the user's score is less than or equal to 3, they will be asked if they want to try the easy set of questions or end the program
+If the user enters Y, go to the "easy" method
+If the user enters N, end the program
+If the user's score is more than or equal to 4, they will be asked if they want to try the hard set of questions or end the program
+If the user enters Y, go to the "hard" method
+If the user enters N, end the program
+If the user's score is less than or equal to 9, they will be asked if they want to try the easy set of questions or end the program
+If the user enters Y, go to the "easy" method
+If the user enters N, end the program
+If the user's score is more than or equal to 10, the program will end
+END
 
 */
 using System;
@@ -22,13 +23,13 @@ namespace MaoriQuiz
 {
     class Program
     {
-        static void correctAns() // Contains the message displayed for correct answers.
+        static void correctAns() // Contains the message displayed for correct answers
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nCongrats, that is correct!");
             Console.ResetColor();
         }
-        static void wrongAns() // Contains the message displayed for wrong answers.
+        static void wrongAns() // Contains the message displayed for wrong answers
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nSorry, that is incorrect.");
@@ -45,19 +46,19 @@ namespace MaoriQuiz
 
             while(true)
             {
-                Console.ForegroundColor = ConsoleColor.Yellow; // Changes text colour.
+                Console.ForegroundColor = ConsoleColor.Yellow; // Changes text colour
                 Console.WriteLine("Hello and welcome to Harman Singh's multiple choice Te Reo Maori quiz! \nWhen asked a question, you must type A, B or C and then press ENTER. \n\nAre you a beginner to Maori? if so, type Y and press ENTER. If not, type N and press ENTER.");
-                Console.ResetColor(); // Resets the text colour to white.
+                Console.ResetColor(); // Resets the text colour to white
                 string choice = Console.ReadLine().ToUpper();
 
 
                 switch (choice)
                 {
                     case "Y":
-                            easy(); // Goes to easy set of questions.
+                            easy(); // Goes to easy set of questions
                             break;
                     case "N":
-                            hard(); // Goes to hard set of questions.
+                            hard(); // Goes to hard set of questions
                             break;
                     default:
                            Console.WriteLine("\nSorry, that is an invalid input. Please type either Y or N and then press ENTER\n");
@@ -66,15 +67,15 @@ namespace MaoriQuiz
                 break;
             } 
 
-            static void easy() // Easy questions.
+            static void easy() // Easy questions
             {
                 int score = 0;
-                while (true)
+                while (true) // Loops forever unless told to break
             { 
                 Console.WriteLine("\nQUESTION 1: \n\nWhat is the Maori word for hello? \n\nA) Ka Pai \nB) Kapa haka \nC) Kia ora\n");
                 string ans1 = Console.ReadLine().ToUpper();
 
-                
+                // Response to user's answer
                 switch (ans1)
                 {
                     case "A":
@@ -82,7 +83,7 @@ namespace MaoriQuiz
                             Console.ForegroundColor = ConsoleColor.Yellow;
                             Console.WriteLine("\nYou have a total of " + score + " points.");
                             Console.ResetColor();
-                            break;
+                            break; // Breaks out of the switch case but not the loop
 
                     case "B":
                             wrongAns();
@@ -101,16 +102,14 @@ namespace MaoriQuiz
 
                     default:
                            invalidAns();
-                           continue;
+                           continue; // Restarts the loop
                 }
-                break;
+                break; // Breaks out of the loop
             }
-
                 while (true)
-                {
+                { 
                     Console.WriteLine("\nQUESTION 2: \n\nWhat is the English phrase for Morena? \n\nA) Good morning \nB) Good afternoon \nC) Good evening\n");
                     string ans2 = Console.ReadLine().ToUpper();
-
 
                     switch (ans2)
                     {
@@ -142,12 +141,10 @@ namespace MaoriQuiz
                     }
                     break;
                 }
-
                 while (true)
                 {
                     Console.WriteLine("\nQUESTION 3: \n\nWhat is the Maori term for performing arts? \n\nA) Kapa haka \nB) Korare \nC) Kaumatua");
                     string ans3 = Console.ReadLine().ToUpper();
-
 
                     switch (ans3)
                     {
@@ -183,7 +180,6 @@ namespace MaoriQuiz
                 {
                     Console.WriteLine("\nQUESTION 4: \n\nMatariki can be best described as... \n\nA) The Maori term for the Pleiades star cluster \nB) The beginning of the Maori New Year \nC) A celebration akin to Christmas");
                     string ans4 = Console.ReadLine().ToUpper();
-
 
                     switch (ans4)
                     {
@@ -221,7 +217,6 @@ namespace MaoriQuiz
                     Console.WriteLine("\nQUESTION 5: \n\nWhat is the Maori term for kanoe? \n\nA) Whaka \nB) Raka \nC) Waka");
                     string ans5 = Console.ReadLine().ToUpper();
 
-
                     switch (ans5)
                     {
                         case "A":
@@ -256,7 +251,6 @@ namespace MaoriQuiz
                 {
                     Console.WriteLine("\nQUESTION 6: \n\nWhat is the English equivalent of Aoteaora? \n\nA) The Pacific Ocean \nB) New Zealand \nC) Atlantis");
                     string ans6 = Console.ReadLine().ToUpper();
-
 
                     switch (ans6)
                     {
@@ -293,7 +287,6 @@ namespace MaoriQuiz
                     Console.WriteLine("\nQUESTION 7: \n\nWhat day is rapare? \n\nA) Thursday \nB) Friday \nC) Saturday");
                     string ans7 = Console.ReadLine().ToUpper();
 
-
                     switch (ans7)
                     {
                         case "A":
@@ -328,7 +321,6 @@ namespace MaoriQuiz
                 {
                     Console.WriteLine("\nFINAL QUESTION: \n\nWhat day follows rapare? \n\nA) Mane \nB) Paraire \nC) Ratapu");
                     string ans8 = Console.ReadLine().ToUpper();
-
 
                     switch (ans8)
                     {
@@ -405,14 +397,13 @@ namespace MaoriQuiz
                 {
                 }
             }
-            static void hard() // Hard questions.
+            static void hard() // Hard questions
             {
                 int score = 0;
                 while (true)
                 {
                     Console.WriteLine("\nQUESTION 1: If you wished to congradulate someone, you would say... \n\nA) Mahi tahi \nB) Nga mihi \nC) Tino pai");
                     string ans1 = Console.ReadLine().ToUpper();
-
 
                     switch (ans1)
                     {
@@ -451,7 +442,6 @@ namespace MaoriQuiz
                     Console.WriteLine("\nQUESTION 2: If you used the greeting tena koe, how many people would you be referring to? \n\nA) One person \nB) Two people \nC) More than three people.");
                     string ans2 = Console.ReadLine().ToUpper();
 
-
                     switch (ans2)
                     {
                         case "A":
@@ -488,7 +478,6 @@ namespace MaoriQuiz
                 {
                     Console.WriteLine("\nNQUESTION 3: If yesterday was stormy, what word would you use to describe the weather? \n\nA) Wera \nB) Makarir \nC) Marangai");
                     string ans3 = Console.ReadLine().ToUpper();
-
 
                     switch (ans3)
                     {
@@ -527,7 +516,6 @@ namespace MaoriQuiz
                     Console.WriteLine("\nQUESTION 4: If I said it was ua outside what would the weather be like? \n\nA) Snowy \nB) Rainy \nC) Hot");
                     string ans4 = Console.ReadLine().ToUpper();
 
-
                     switch (ans4)
                     {
                         case "A":
@@ -564,7 +552,6 @@ namespace MaoriQuiz
                 {
                     Console.WriteLine("\nQUESTION 5: What is the Maori word for bus? \n\nA) Pahi \nB) Mahi \nC) Paki");
                     string ans5 = Console.ReadLine().ToUpper();
-
 
                     switch (ans5)
                     {
@@ -603,7 +590,6 @@ namespace MaoriQuiz
                     Console.WriteLine("\nQUESTION 6: If I said I came home via tereina, what did I come home in? \n\nA) Train \nB) Bus \nC) Car");
                     string ans6 = Console.ReadLine().ToUpper();
 
-
                     switch (ans6)
                     {
                         case "A":
@@ -641,7 +627,6 @@ namespace MaoriQuiz
                     Console.WriteLine("\nQUESTION 7: Which is the correct Maori translation of the sentence: \nHello everyone to Avondale College. \n\nA) Kia ora koutou katoa o Avondale College. \nB) Kia ora ki tetahi o te Kareti o Avondale. \nC) Kia ora ki te tokorua kei Avondale College");
                     string ans7 = Console.ReadLine().ToUpper();
 
-
                     switch (ans7)
                     {
                         case "A":
@@ -676,9 +661,8 @@ namespace MaoriQuiz
                 }
                 while (true)
                 {
-                    Console.WriteLine("\nQUESTION 7: Which is the correct Maori translation of the sentence: \nHello everyone to Avondale College. \n\nA) Kia ora koutou katoa o Avondale College. \nB) Kia ora ki tetahi o te Kareti o Avondale. \nC) Kia ora ki te tokorua kei Avondale College");
+                    Console.WriteLine("\nFINAL QUESTION: Complete the sentence: I oma ia i te tere o te... \n(He ran at the speed of light) \n\nA) Mama \nB) Mana \nC) Marama");
                     string ans7 = Console.ReadLine().ToUpper();
-
 
                     switch (ans7)
                     {
